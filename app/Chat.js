e321bee95301130db84625551e244530b00f1143
oguzcanduru <duruoguz8@gmail.com>
2024-08-16 18:09:59 +0300

    smkm

--- app/Chat.js
+++ app/Chat.js
@@ -1,9 +1,12 @@
-import { View, Text } from "react-native";
+import { router } from "expo-router";
+import { View, Text, TouchableOpacity } from "react-native";
 
 const Chat = () => {
     return (
-        <View>
-            <Text>hebhebeh</Text>
+        <View style={{justifyContent:'center', backgroundColor: 'red', width: '100%', height:100, }}>
+            <TouchableOpacity onPress={()=>{router.canGoBack()}}>
+            <Text>go back</Text>
+            </TouchableOpacity>
         </View>
     );
 }
